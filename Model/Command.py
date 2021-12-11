8da3fce326d1f405b772631343c2c66a4270c039
kalus <kaluski1212@wp.pl>
2021-12-11 12:50:24 +0100

    Fix command

--- Model/Command.py
+++ Model/Command.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 import datetime
 
-#actions
-
-#get_captures_list = "get_captures_list"
-#get_captures = "get_captures"
-#get_logs_list = "get_logs_list"
-#get_logs = "get_logs"
+# actions
 get_configuration = "get_configuration"
+# get_captures_list = "get_captures_list"
+# get_captures = "get_captures"
+# get_logs_list = "get_logs_list"
+# get_logs = "get_logs"
 capture_traffic = "capture_traffic"
 execute_command = "execute_command"
 
@@ -23,26 +23,11 @@ parameter: parameters for commands - default: ""
         .
         .
 '''
+
+
 class Command(BaseModel):
-        action: str 
-        names: str 
-        time: int 
-        parameters: str 
-
-        def __init__(self, action = "n" , names = "n", time=30, parameters="n"):
-            self.__action =action
-            self.__names = names
-            self.__time = time
-            self.__parameters = parameters
-
-
-        def set_action(self, x):
-                self.__action= x
-        def set_time(self, x):
-                self.__time = x
-        def set_parameters(self, x):
-                self.__parameters = x
-        def set_names(self, x):
-                self.__names = x
+    action: str
+    time: Optional[int] = None
+    parameters: Optional[str] = None
 
 
